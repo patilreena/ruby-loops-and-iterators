b87019da73f08e3ecd9b52b59295e8b83f704d89
Reena Patil <reena.patil@outlook.com>
2017-09-06 14:41:09 +1000

    ruby while loop with example

---
+++ while.rb
@@ -0,0 +1,10 @@
+# It checks to see if a certain condition is true, and while it is,
+# the loop keeps running. As soon as the condition stops being true,
+# the loop stops!
+
+
+x = 1
+while x < 11
+  puts x
+  x = x + 1
+end
