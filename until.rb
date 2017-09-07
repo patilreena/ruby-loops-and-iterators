078b455abe41b4a3fef38b25942f41f52a0f46ac
Reena Patil <reena.patil@outlook.com>
2017-09-07 11:27:01 +1000

    rubu until loop

---
+++ until.rb
@@ -0,0 +1,17 @@
+# The complement to the while loop is the until loop.
+#  It's sort of like a backward while:
+
+counter = 1
+until counter > 10
+  puts counter
+  # Add code to update 'counter' here!
+  counter += 1
+end
+
+# In the example above, we first create a variable called counter
+# and set it to 1.
+
+# Then we execute a block of code until counter is greater than 10.
+#That block of code increments counter
+
+# When counter is equal to or greater than 10, the block ends.
