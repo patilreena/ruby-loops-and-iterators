b854577a81c6d87e07dc3101d39e0212324e4f7b
Reena Patil <reena.patil@outlook.com>
2017-09-07 10:51:14 +1000

    inclusive and exclusive in ruby

---
+++ inclusive.rb
@@ -0,0 +1,10 @@
+ # If we use two dots, this tells Ruby to include the highest number in the
+ #range.
+
+ #if we use three dots,it tells Ruby to exclude the final number in the count:
+ #for num in 1...10 means "go up to but don't include 10.
+
+
+ for num in 1...10
+   puts num
+ end
