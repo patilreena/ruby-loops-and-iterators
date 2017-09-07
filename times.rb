bb25cad9a9bbac6ba0d0d4f511f09556057adc49
Reena Patil <reena.patil@outlook.com>
2017-09-07 11:25:10 +1000

    ruby times method

---
+++ times.rb
@@ -0,0 +1,7 @@
+# The .times method is like a super compact for loop: it can perform a task on
+# each item in an object a specified number of times.
+#
+# For example, if we wanted to print out our name ten times, we might type
+
+
+10.times { puts "my name is..." }
